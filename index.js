bb79808dcdb57a532444dce4be343faf115a0f62
Antonyxit <carlosmorga45@gmail.com>
2024-08-28 22:24:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('𝐏𝐡𝐢𝐚\nBot', {
+say('Princesa\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Monovan`, {
+say(`Bot creado por Antonyy`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
