0b315c202fa822b70995b0b9d97462ef2163300c
Antonyxit <carlosmorga45@gmail.com>
2024-08-29 20:01:56 -0600

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -32,11 +32,11 @@ const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, i
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `𝐃𝐚𝐧𝐧𝐲 𝐁𝐎𝐓 𝐏𝐑𝐎
+    const str = `𝐏𝐢𝐤𝐚𝐜𝐡𝐮 𝐁𝐎𝐓 𝐏𝐑𝐎
 
 ${tradutor.texto1[1]} ${taguser}
 
-> ★ 𝐃𝐚𝐧𝐧𝐲 BOT 🌴★
+> ★ 𝐏𝐢𝐤𝐚𝐜𝐡𝐮 BOT 🌴★
 
 ${tradutor.texto1[2]}
 
